866f2e695019b3dbbdea4d9508305855ade0f1bb
Haniyeh Ehsani <45337101+aheldis@users.noreply.github.com>
2023-04-30 02:05:33 +0330

    2.5 times

--- scripts/kitti_evaluation.py
+++ scripts/kitti_evaluation.py
@@ -120,7 +120,7 @@ def make_kitti_submission(model):
                 epsilon = args.epsilon
                 pgd_iters = 1
             else:
-                epsilon = args.epsilon / args.iters
+                epsilon = 2.5 * args.epsilon / args.iters
                 pgd_iters = args.iters
 
             ori = image1.data
